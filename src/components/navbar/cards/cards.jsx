ce013921e03866ac8b2e9c0c589a3f94f1f6a5cb
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2024-02-07 19:16:36 -0300

    addFavourites and deleteFavourites

--- src/components/navbar/cards/cards.jsx
+++ src/components/navbar/cards/cards.jsx
@@ -1,8 +1,40 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import styles from './cards.module.css';
 
 const Cards = ({name, img, extension}) => {
   const [inFavourite, setInFavourite] = useState (false);
+
+  //Chequea al montar la card que se encuentre en favoritos
+  useEffect (() => {
+    let favourite = JSON.parse (localStorage.getItem ('Favourites')) || [];
+    let pos = favourite.findIndex (el => el.name == name);
+    if (pos > -1) {
+      setInFavourite (true);
+    }
+  }, []);
+
+  //Agrega a favoritos
+  const AddFavourites = title => {
+    let favourite = JSON.parse (localStorage.getItem ('Favourites')) || [];
+    let pos = favourite.findIndex (el => el.name == title);
+    if (pos > -1) {
+      //   localStorage.setItem("Favourites", JSON.stringify(favourite));
+      //poner un alert que ya esta el producto
+    } else {
+      favourite.push ({name});
+      localStorage.setItem ('Favourites', JSON.stringify (favourite));
+      setInFavourite (true);
+    }
+  };
+
+  //Elimina de favoritos a la card
+  const DeleteFavourites = title => {
+    let favourite = JSON.parse (localStorage.getItem ('Favourites')) || [];
+    let favoriteFilter = favourite.filter (el => el.name !== title);
+    localStorage.setItem ('Favourites', JSON.stringify (favoriteFilter));
+    setInFavourite (false);
+  };
+
 
   return (
     <div className={styles.contain}>
@@ -19,7 +51,7 @@ const Cards = ({name, img, extension}) => {
               fill="none"
               stroke-linecap="round"
               stroke-linejoin="round"
-              onClick={() => setInFavourite (true)}
+              onClick={() => AddFavourites (name)}
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
@@ -35,7 +67,7 @@ const Cards = ({name, img, extension}) => {
               fill="gainsboro"
               stroke-linecap="round"
               stroke-linejoin="round"
-              onClick={() => setInFavourite (false)}
+              onClick={() => DeleteFavourites (name)}
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path
