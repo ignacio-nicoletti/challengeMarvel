96c861f11c1af385361569cd354d6937253b59a3
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2024-02-07 15:00:12 -0300

    map of data and filter for searchbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import {useEffect, useState} from 'react';
 
 import {getChars} from '../../utils/fetchChars';
+import Navbar from '../../components/navbar/navbar';
+import Cards from '../../components/navbar/cards/cards';
+import styles from './Home.module.css';
 
 const Home = () => {
-  const [data, setdata] = useState ([]);
+  const [data, setdata] = useState ([]); //se almacena toda la data
+  const [search, setSearch] = useState (''); //se almacena la palabra de busqueda
+  const [datafilter, setDatafilter] = useState (data); //se almacena la data filtrada
 
   useEffect (() => {
     CallCharacter ();
@@ -14,10 +19,38 @@ const Home = () => {
     setdata (data);
   };
 
-  console.log (data);
+  useEffect (
+    () => {
+      const datafiltered = data.filter (el =>
+        el.name.toLowerCase ().includes (search)
+      );
+      setDatafilter (datafiltered);
+    },
+    [search]
+  );
+
   return (
     <div>
-      <h1>Home</h1>
+      <Navbar search={search} setSearch={setSearch} />
+
+      <div className={styles.cardContain}>
+        {search !== ''
+          ? datafilter.map (el => (
+              <Cards
+                name={el.name}
+                img={el.thumbnail.path}
+                extension={el.thumbnail.extension}
+              />
+            ))
+          : data.map (el => (
+              <Cards
+                name={el.name}
+                img={el.thumbnail.path}
+                extension={el.thumbnail.extension}
+              />
+            ))}
+      </div>
+
     </div>
   );
 };
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,61 @@
+import styles from './navbar.module.css';
+import logoMarvel from '../../assets/logomarvel.png';
+
+const Navbar = ({search,setSearch}) => {
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value.toLowerCase());
+  };
+
+
+  return (
+    <div className={styles.contain}>
+      <div className={styles.logo}>
+        <img src={logoMarvel} alt="" />
+      </div>
+
+      <div className={styles.searchbar}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-search"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="gray"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+          <path d="M21 21l-6 -6" />
+        </svg>
+
+        <input type="text" placeholder="Buscar" value={search} onChange={handleSearch}/>
+
+      </div>
+
+      <div className={styles.favourite}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-star"
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+          stroke-width="1"
+          stroke="gray"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+        </svg>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
