788545c5e5fb5984b9602428b2a3afbba2f7fe01
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2024-02-08 09:18:13 -0300

    fix css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import styles from './footer.module.css';
+import linkedinIcon from '../../assets/linkedin.png';
+import githubIcon from '../../assets/github.png';
+
 const Footer = () => {
   return (
     <div className={styles.contain}>
-    
 
-        <p>Proyecto realizado por Nicoletti Ignacio</p>
-     
+      <p>Proyecto realizado por Nicoletti Ignacio</p>
+      <div className={styles.social}>
+        <a
+          href="https://www.linkedin.com/in/ignacio-nicoletti/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedinIcon} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/ignacio-nicoletti"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+      </div>
+
     </div>
   );
 };
